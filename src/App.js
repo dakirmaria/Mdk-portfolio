18024c23693b182a0118f65aa083cf793a2f3f31
maria dakir <m.dakir@mundiapolis.ma>
2023-03-01 14:18:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import { Repositories } from "./components/Repositories";
 function App() {
-  console.log("this is from app " + process.env.REACT_APP_BASE_URL            )
 
   return (
     <main  className="text-gray-400 bg-gray-900 body-font">
